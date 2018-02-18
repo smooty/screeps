e32141ed442f0cdd008379732fb7d902c2dee91c
smooty <jason.stoops@gmail.com>
2018-02-17 21:24:03 -0800

    2018 init commit

--- main.js
+++ main.js
@@ -1,74 +1,34 @@
-/**
-    https://github.com/smooty/screeps
-    Summer, 2016
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
-    TO FIX:  Clean up the tower defense code so that I'm not hard coding the room ID
-*/
+module.exports.loop = function () {
 
-"use strict";
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
 
-// global variables
-var objRoles = require('mod.roles');
-var objFunctions = require('mod.functions');
-
-// main function
-module.exports.loop = function ()
-{
-    // can i tell if different rooms are being cycled through in this function?
-    for (let i in Game.rooms)
-    {
-        var objRoom = Game.rooms[i];
-        // console.log(objRoom.name);
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
     }
 
-    // spawn new creeps if necessary
-    objFunctions.spawn_new_creeps();
-
-    // for each of the creeps...
-    for (let name in Game.creeps)
-    {
+    for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-
-        // if creep is builder...
-        if (creep.memory.role == 'builder')
-        {
-            objRoles.builder(creep);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
         }
-        // if creep is harvester...
-        else if (creep.memory.role == 'harvester')
-        {
-            objRoles.harvester(creep);
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
         }
-        // if creep is mason...
-        else if (creep.memory.role == 'mason')
-        {
-            objRoles.mason(creep);
-        }
-        // if creep is repairer...
-        else if (creep.memory.role == 'repairer')
-        {
-            objRoles.repairer(creep);
-        }
-        // if creep is scout harvester...
-        else if (creep.memory.role == 'scout_harvester')
-        {
-            objRoles.scout_harvester(creep);
-        }
-        // if creep is upgrader...
-        else if (creep.memory.role == 'upgrader')
-        {
-            objRoles.upgrader(creep);
-        }
-        // if creep is settler...
-        else if (creep.memory.role == 'settler')
-        {
-            objRoles.settler(creep);
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
-
-    // tower defense
-    objFunctions.tower_defense();
-
-    // manage memory
-    objFunctions.manage_memory();
-};
+}
